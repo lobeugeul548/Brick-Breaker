33c74013781f4066c112fb067fe7b40487bde3f8
lobeugeul548 <92210086+lobeugeul548@users.noreply.github.com>
2022-10-12 22:23:21 +0900

    다만듬

--- Brick Breaker(Base).py
+++ Brick Breaker(Base).py
@@ -24,7 +24,7 @@ class Bar:
         self.start_y = 870
         self.x = 300
         self.y = 25
-        self.speed = 4
+        self.speed = 1
         self.color = random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)
 
     def draw_bar(self):
@@ -36,7 +36,7 @@ class Ball:
     def __init__(self):
         self.ball_img = pygame.image.load("진짜 공.png")
         self.ball_img = pygame.transform.scale(self.ball_img, (50, 50))
-        self.speed = 5
+        self.speed = 1
         self.start_x = 575
         self.start_y = 810
         self.direction = 0
@@ -72,7 +72,25 @@ clock = pygame.time.Clock()
 Quit = False
 clock.tick(60)
 
+#게임 대기화면
+SB = 0
+while SB == 0:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                SB = 1
+    screen.fill(BLACK)
+    font = pygame.font.Font("C:/Users/배유찬/AppData/Local/Microsoft/Windows/Fonts/neodgm.ttf", 30)
+    text = font.render("PRESS SPACE KEY TO START THE GAME", True, (255, 0, 0))
+    screen.blit(text, (350, round(size[1] / 2 - 50)))
+    pygame.display.flip()
+
 # 메인 루프
+GO = 0
 bar = Bar()
 brick_list = []
 ball = Ball()
@@ -125,6 +143,11 @@ while not Quit:
     elif ball.start_y <= 0:
         ball.move_y = -ball.move_y
 
+    # 공이 땅에 떨어졌을떄
+    elif ball.start_y > 900:
+        GO = 1
+        Quit = True
+
     # 바에 부딪혔을때
     if ball.start_y >= 820 and (bar.start_x <= ball.start_x <= (bar.start_x + 300)): # 윗쪽
         ball.move_y = -ball.move_y
@@ -164,5 +187,15 @@ while not Quit:
     pygame.display.flip()
 
 # pygame, 밑 system 종료
+while GO == 1:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    font = pygame.font.Font("C:/Users/배유찬/AppData/Local/Microsoft/Windows/Fonts/neodgm.ttf", 120)
+    text = font.render("GAME OVER", True, (255, 0, 0))
+    screen.blit(text, (330, round(size[1] / 2 - 80)))
+    pygame.display.flip()
 pygame.quit()
 sys.exit()
